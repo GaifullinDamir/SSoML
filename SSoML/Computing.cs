4ed857d77d5c2512003ad2c61b36a38b72e4cf50
Gaifullin Damir <101250473+GaifullinDamir@users.noreply.github.com>
2022-05-17 23:15:12 +0300

    Добавьте файлы проекта.

---
+++ SSoML/Computing.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program
+{
+    public class Computing
+    {
+        public int K { get; set; }
+        public int N { get; set; }
+
+        private const string TD = "_"; //отрицание Поста
+        private const string NP = "-."; //усечённая разность
+
+        private static List<char> permittedSymbolsForFunction = new List<char>() { '(', ')', '_', '/', 'x', 'y' };
+        private static HashSet<string> forbiddenCombs = new HashSet<string>() { "", "_)", "(_", "/_", "xy", "yx" };
+
+        private int[] firstColumn;
+        private int[] secondColumn;
+        public Computing()
+        {
+
+        }
+        public static int PostsNegation(int x, int k)
+        {
+            return (x + 1) % k;
+        }
+
+        public static int TrunDifference(int x, int y)
+        {
+            if (x >= y)
+            {
+                return x - y;
+            }
+            else
+                return 0;
+        }
+
+        public static void CreateColumns(int k, ref int[] firstColumn)
+        {
+            firstColumn = new int[k];
+            for (int i = 0; i < k; i++)
+            {
+                firstColumn[i] = i;
+            }
+        }
+
+        public static void CreateColumns(int k, ref int[] firstColumn, ref int[] secondColumn)
+        {
+            firstColumn = new int[k*k];
+            secondColumn = new int[k*k];
+            int step = 0;
+            for (int i = 0; i < k; i++)
+            {
+                for (int j = 0; j < k; j++)
+                {
+                    firstColumn[step] = i;
+                    secondColumn[step] = j;
+                    step++;
+                }
+            }
+        }
+    }
+}
---
+++ SSoML/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace program
+{
+    public class Interface
+    {
+        public static int IntInput()
+        {
+            string strInput; bool stop = false;
+            int number = -1;
+            while (!stop)
+            {
+                try
+                {
+                    strInput = Console.ReadLine();
+                    number = int.Parse(strInput); stop = true;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Не верный ввод.");
+                    stop = false;
+                    break;
+                }
+            }
+            return number;
+        }
+    }
+    class Program
+    {
+        int k;
+        int n;
+        public static int[] firstColumn;
+        public static int[] secondColumn;
+
+        static void Main(string[] args)
+        {
+
+
+            Computing.CreateColumns(7, ref firstColumn, ref secondColumn);
+            for (int i = 0; i < 49; i++)
+            {
+                Console.WriteLine($" {firstColumn[i]}   {secondColumn[i]}");
+            }
+        }
+
+        
+    }
+}
